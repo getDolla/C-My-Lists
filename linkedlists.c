02683fd02e4a7f23419990d7b87608f9d538d0ae
getDolla <ywang8@stuy.edu>
2016-10-07 21:24:34 -0400

    Updated

---
+++ linkedlists.c
@@ -0,0 +1,40 @@
+/*
+Yikai Wang
+Period 4 Systems
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node { int i; struct node *next; };
+
+void print_list( struct node *n ) {
+	struct node *front = n;
+	while( front != NULL ) {
+		printf( "%d -> ", front->i );
+		front = front->next;
+	}
+
+	printf( "END\n" );
+}
+
+void main() {
+	/*
+	int i = 0;
+	int *p = &i;
+	*p = 1;
+	printf( "i: %d\n", i );
+	printf( "p: %d\n", *p );
+	*/
+
+	struct node *n = (struct node *)malloc(sizeof(struct node));
+	n->i = 10;
+	struct node *o = (struct node *)malloc(sizeof(struct node));
+	o->i = 15;
+	n->next = o;
+
+	//struct node *front = n;
+	//printf( "%d\n", (front->next == NULL));
+
+	print_list(n);
+}
