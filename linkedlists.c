0000b3a7ad1d229616b5e1b22b03b09e7d3582cc
getDolla <ywang8@stuy.edu>
2016-10-09 10:17:28 -0400

    Update with insert_front

--- linkedlists.c
+++ linkedlists.c
@@ -10,13 +10,20 @@ struct node { int i; struct node *next; };
 
 void print_list( struct node *n ) {
 	struct node *front = n;
-	while( front ) {
+	while( front ) { //while not a NULL value
 		printf( "%d -> ", front->i );
 		front = front->next;
 	}
 
 	printf( "END\n" );
 }
+
+struct node* insert_front( struct node *prev, int data ) {
+	struct node *start = (struct node *)malloc(sizeof(struct node));
+	start->i = data;
+	start->next = prev;
+	return start;
+}  
 
 void main() {
 	/*
@@ -36,5 +43,17 @@ void main() {
 	//struct node *front = n;
 	//printf( "%d\n", (front->next == NULL));
 
+	printf( "Testing: print_list:\n" );
+	print_list(n);
+
+	printf( "Adding nodes to the front:\n");
+	
+	printf("Adding: 5\n");
+	n = insert_front( n, 5 );
+
+	printf("Adding: 0\n");
+	n = insert_front( n, 0 );
+
+	printf( "Testing: print_list (again):\n" );
 	print_list(n);
 }
