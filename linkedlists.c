9eea67b839774f11200f9247b975f9a5500b9091
getDolla <ywang8@stuy.edu>
2016-10-09 13:11:13 -0400

    All done

--- linkedlists.c
+++ linkedlists.c
@@ -6,7 +6,7 @@ Period 4 Systems
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node { int i; struct node *next; };
+struct node { int i; struct node *next; }; //protoype struct
 
 void print_list( struct node *n ) {
 	struct node *front = n;
@@ -19,11 +19,21 @@ void print_list( struct node *n ) {
 }
 
 struct node* insert_front( struct node *prev, int data ) {
-	struct node *start = (struct node *)malloc(sizeof(struct node));
-	start->i = data;
+	struct node *start = (struct node *)malloc(sizeof(struct node)); //new node
+	start->i = data; //insert data
 	start->next = prev;
 	return start;
-}  
+}
+
+struct node* free_list( struct node* start ) {
+	struct node* iterate = start;
+	while( iterate ) { //if not a null
+		free( iterate );
+		iterate = iterate->next;
+	}
+
+	return start;
+}
 
 void main() {
 	/*
@@ -35,9 +45,9 @@ void main() {
 	*/
 
 	struct node *n = (struct node *)malloc(sizeof(struct node));
-	n->i = 10;
+	n->i = 15;
 	struct node *o = (struct node *)malloc(sizeof(struct node));
-	o->i = 15;
+	o->i = 20;
 	n->next = o;
 
 	//struct node *front = n;
@@ -46,8 +56,11 @@ void main() {
 	printf( "Testing: print_list:\n" );
 	print_list(n);
 
-	printf( "Adding nodes to the front:\n");
+	printf( "\nAdding nodes to the front:\n");
 	
+	printf("Adding: 10\n");
+	n = insert_front( n, 10 );
+
 	printf("Adding: 5\n");
 	n = insert_front( n, 5 );
 
@@ -55,5 +68,13 @@ void main() {
 	n = insert_front( n, 0 );
 
 	printf( "Testing: print_list (again):\n" );
+	print_list(n);
+
+	printf("\nFreeing the list...\n");
+	n = free_list(n);
+
+	printf("Is the beginning of the list null? %d\n", (n == NULL) );
+
+	printf("Testing: print_list (yet again):\n");
 	print_list(n);
 }
